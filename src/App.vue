51fe3a25b18526b6bf53dd428cf4f1a00d3a9096
Dominic <dolearczyk@mail.de>
2022-12-23 16:12:23 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-6">
+        test
+      </div>
+      <div class="col-6">
+        test2
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css';
+@import 'https://wms.acut-services.de/css/app.css';
+
+body {
+  margin:0;
+  padding:20px;
+}
+</style>
